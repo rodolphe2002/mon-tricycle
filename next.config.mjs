2514477237ad97a60e579b31548e95e154b09763
Kouadio Bah Rodolphe <rodolphekouadio225@icloud.com>
2025-08-26 08:27:08 +0000

    configuration techniques

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    // Ignore ESLint errors during production builds
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
     config.module.rules.push({
       test: /\.(glsl|vs|fs|vert|frag)$/,
